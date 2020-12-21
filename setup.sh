fee024e5ba78be07a54ec575697049563956c831
Shyam Sudhakaran <shyamsnair@protonmail.com>
2020-12-21 12:09:44 -0800

    cleaned some files

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ git clone https://github.com/real-itu/Evocraft-py.git temp
 rsync -a temp/* evocraft_ga/external/
 rm -rf temp
 cd evocraft_ga/external/ && python edit_file.py --file_path=minecraft_pb2_grpc.py --find_str="import minecraft_pb2" --replace_str="import evocraft_ga.external.minecraft_pb2"
+cd ../../
+python setup.py install
